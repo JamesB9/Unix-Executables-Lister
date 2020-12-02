85ff9c4eac0873b9e8228d39f93a0d6e7226bd96
J-B <57415099+J-Burling@users.noreply.github.com>
2020-12-02 14:53:27 +0000

    working threads

--- main.c
+++ main.c
@@ -2,13 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <pthread.h>
 #include <dirent.h>
 #include <sys/stat.h>
 #include "StringList/StringList.h"
 
 #define TRUE 1
 #define FALSE 0
+#define MAX_PATH_SIZE 256
 
+
+
+/* Structure: Workspace
+ * Holds all variables required for each worker thread.
+ *
+ * @variable directory   - String containing the path of the directory to search in
+ */
+typedef struct WorkspaceStruct{
+    char* directory;
+}Workspace;
 
 int isExecutableFile(const char* filePath){
     // Access File's Attributes
@@ -23,27 +35,47 @@ int isExecutableFile(const char* filePath){
     return FALSE;
 }
 
-void listExecutables(StringList* directories){
-    for(int i = 0; i < directories->length; i++){
-        DIR *dir = opendir(get(directories, i)); // Open Directory
+void* listExecutables(void *ws){
+    Workspace *workspace = (Workspace*) ws;
 
-        if(dir == NULL){
-            fprintf(stderr, "The directory '%s' doesn't exist\n", get(directories, i));
-        }else{
-            struct dirent *dirEntry;
-            int fileCounter = 0;
-            while ((dirEntry = readdir(dir)) != NULL){
-                // Get Full File Path
-                char filePath[256];
-                snprintf(filePath,256,"%s/%s", get(directories, i), dirEntry->d_name);
+    DIR *dir = opendir(workspace->directory); // Open Directory
 
-                if(isExecutableFile(filePath)){
-                    printf("%s\n", filePath);
-                }
+    if(dir == NULL){
+        fprintf(stderr, "The directory '%s' doesn't exist\n", workspace->directory);
+    }else{
+        struct dirent *dirEntry;
+        int fileCounter = 0;
+        while ((dirEntry = readdir(dir)) != NULL){
+            // Get Full File Path
+            char filePath[MAX_PATH_SIZE];
+            snprintf(filePath, MAX_PATH_SIZE,"%s/%s", workspace->directory, dirEntry->d_name);
+
+            if(isExecutableFile(filePath)){
+                printf("%s\n", filePath);
             }
         }
+    }
 
-        closedir(dir); // Close Directory
+    closedir(dir); // Close Directory
+    return NULL;
+}
+
+
+void listExecsFromDirectories(StringList* list){
+    Workspace workspaces[list->length];
+    pthread_t workerThreads[list->length];
+
+    for(int i = 0; i < list->length; i++) {
+        workspaces[i].directory = get(list, i);
+
+        int status = pthread_create(&(workerThreads[i]), NULL, listExecutables, &workspaces[i]);
+        if(status != 0){
+            perror("Error creating Thread: ");
+        }
+    }
+
+    for(int i = 0; i < list->length; i++) {
+        pthread_join(workerThreads[i], NULL);
     }
 }
 
@@ -57,8 +89,8 @@ void listExecutables(StringList* directories){
  * @return int of how the program exited.
  */
 int main(int argc, char *argv[]) {
-    // Create and Initialise a StringList to hold the directories
-    StringList* directories = NULL;
+    StringList* directories;
+
     // CREATING LIST OF DIRECTORIES FROM ARGUMENTS //
     if(argc == 1){ // No args = Use current working directory
         directories = initStringList();
@@ -72,7 +104,7 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    listExecutables(directories);
+    listExecsFromDirectories(directories);
     freeStringList(directories);
     return EXIT_SUCCESS;
 }
